1faad562daa6dc1af638ef460ba51569b5000f86
CedricCampagne <ced.campagne@gmail.com>
2026-07-24 13:29:04 +0200

    feat: add projects with API integreation(cors config)

--- front/src/app/home/components/projects/projects.ts
+++ front/src/app/home/components/projects/projects.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ProjectService } from '../../../projects/services/project.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class Projects {}
+export class Projects {
+
+  private projectService = inject(ProjectService);
+
+
+  projects = toSignal(
+    this.projectService.getProjects(),
+    {
+      initialValue: []
+    }
+  );
+
+}
