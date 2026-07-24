a370d51b2cbe761800dca2e81b7cf5db7d7b8ed2
CedricCampagne <ced.campagne@gmail.com>
2026-07-24 12:47:30 +0200

    feat: add skills component

--- front/src/app/home/components/skills/skills.ts
+++ front/src/app/home/components/skills/skills.ts
@@ -2,8 +2,82 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
+  standalone: true,
   imports: [],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
-export class Skills {}
+export class Skills {
+
+  categories = [
+    'Frontend',
+    'Backend',
+    'Database',
+    'Outils'
+  ];
+
+  skills = [
+
+    // Frontend
+    {
+      name: 'Angular',
+      category: 'Frontend',
+      icon: 'logos:angular-icon'
+    },
+    {
+      name: 'TypeScript',
+      category: 'Frontend',
+      icon: 'logos:typescript-icon'
+    },
+    {
+      name: 'HTML / CSS',
+      category: 'Frontend',
+      icon: 'logos:html-5'
+    },
+    {
+      name: 'Tailwind CSS',
+      category: 'Frontend',
+      icon: 'logos:tailwindcss-icon'
+    },
+    // Backend
+    {
+      name: 'Java',
+      category: 'Backend',
+      icon: 'logos:java'
+    },
+    {
+      name: 'Spring Boot',
+      category: 'Backend',
+      icon: 'logos:spring-icon'
+    },
+    {
+      name: 'REST API',
+      category: 'Backend',
+      icon: 'mdi:api'
+    },
+    // Database
+    {
+      name: 'PostgreSQL',
+      category: 'Database',
+      icon: 'logos:postgresql'
+    },
+    // Outils
+    {
+      name: 'Git',
+      category: 'Outils',
+      icon: 'logos:git-icon'
+    },
+    {
+      name: 'GitHub',
+      category: 'Outils',
+      icon: 'mdi:github'
+    },
+    {
+      name: 'Postman',
+      category: 'Outils',
+      icon: 'logos:postman-icon'
+    }
+
+  ];
+
+}
