310739ae9dc6c79481baefdbe20c20608b38446f
CedricCampagne <ced.campagne@gmail.com>
2026-07-27 09:39:56 +0200

    feat: add project details page

--- front/src/app/projects/project-details/project-details.ts
+++ front/src/app/projects/project-details/project-details.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-project-details',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './project-details.html',
   styleUrl: './project-details.css',
 })
-export class ProjectDetails {}
+export class ProjectDetails {
+
+  private route = inject(ActivatedRoute);
+  private projectService = inject(ProjectService);
+
+  readonly project = toSignal(
+    this.route.paramMap.pipe(
+      switchMap(params =>
+        this.projectService.getProjectsBySlug(params.get('slug')!)
+      )
+    ),
+    {
+      initialValue: null
+    }
+  );
+
+  protected isArray(value: unknown): value is string[] {
+    return Array.isArray(value);
+  }
+
+}
