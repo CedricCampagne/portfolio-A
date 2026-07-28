54c3835161431fa1bff0d0b69e492e0e6c2c9147
CedricCampagne <ced.campagne@gmail.com>
2026-07-28 14:15:01 +0200

    allow Vercel frontend origin

--- api/src/main/java/fr/cedric/api/config/CorsConfig.java
+++ api/src/main/java/fr/cedric/api/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://portfolio-2gyytweca-cedriccampagnes-projects.vercel.app"
+                        )
                         .allowedMethods("GET")
                         .allowedHeaders("*");
             }
