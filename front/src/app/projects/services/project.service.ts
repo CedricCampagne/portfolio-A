adbff8b7442b19982a3e14b733e497b56df5453d
CedricCampagne <ced.campagne@gmail.com>
2026-07-28 13:45:57 +0200

    add Angular environment config for API endpoints

--- front/src/app/projects/services/project.service.ts
+++ front/src/app/projects/services/project.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable, } from "@angular/core";
 import { Project } from "../models/project.model";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class ProjectService {
 
     constructor(private http: HttpClient){}
 
-    private readonly apiUrl = "http://localhost:8080/api/projects";
+    private readonly apiUrl = `${environment.apiUrl}/api/projects`;
 
     getProjects(): Observable<Project[]>{
         return this.http.get<Project[]>(this.apiUrl);
