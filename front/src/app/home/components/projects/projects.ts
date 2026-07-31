f5c7470924928c13b5f0877b64efef46969b646a
CedricCampagne <ced.campagne@gmail.com>
2026-07-31 10:01:09 +0200

    feat(front): add loading state for projects and projects details

--- front/src/app/home/components/projects/projects.ts
+++ front/src/app/home/components/projects/projects.ts
@@ -1,7 +1,8 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ProjectService } from '../../../projects/services/project.service';
 import { RouterLink } from '@angular/router';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
@@ -16,16 +17,19 @@ export class Projects {
 
   private projectService = inject(ProjectService);
 
+  isLoading = signal(true);
 
   projects = toSignal(
-    this.projectService.getProjects(),
+    this.projectService.getProjects().pipe(
+      finalize(() => this.isLoading.set(false))
+    ),
     {
       initialValue: []
     }
   );
 
   projectsSorted = computed (()=>
-    [...this.projects()].reverse()
+    [...this.projects()].sort((a,b) => b.id - a.id)
   );
 
 }
