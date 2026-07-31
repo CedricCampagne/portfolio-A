cfc8f48fd19b6195d6acffd138634f9051d793f3
CedricCampagne <ced.campagne@gmail.com>
2026-07-31 16:03:03 +0200

    feat: add SEO meta tags and convert project images to WebP

---
+++ api/src/main/resources/db/migration/V5__update_project_images_to_webp.sql
@@ -0,0 +1,11 @@
+UPDATE projects
+SET image = '/images/site-auteur.webp'
+WHERE slug = 'site-auteur';
+
+UPDATE projects
+SET image = '/images/portfolio.webp'
+WHERE slug = 'portfolio';
+
+UPDATE projects
+SET image = '/images/shopping-recipes.webp'
+WHERE slug = 'shopping-recipes';
